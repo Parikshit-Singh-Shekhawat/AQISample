8e8fecc26bcd1edbd8e4e2a300fafb1237ccc5c3
sds-s.parikshit <s.parikshit@partner.samsung.com>
2021-10-10 10:29:00 +0530

    WebSocket changes commit

--- app/build/generated/data_binding_trigger/debug/com/airindexapp/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.airindexapp;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
--- app/build/generated/source/kapt/debug/com/airindexapp/BR.java
+++
@@ -1,7 +0,0 @@
-package com.airindexapp;
-
-public class BR {
-  public static final int _all = 0;
-
-  public static final int data = 1;
-}
